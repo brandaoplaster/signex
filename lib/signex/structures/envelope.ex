2e11c82fb120b91eadcc0bcfd256809dda30a1f2
brandaoplaster <brandaoplaster@gmail.com>
2025-03-16 14:52:14 -0300

    feat: Add type and spec

--- lib/signex/structures/envelope.ex
+++ lib/signex/structures/envelope.ex
@@ -1,4 +1,8 @@
 defmodule Signex.Structures.Envelope do
+  @moduledoc """
+
+  """
+
   @enforce_keys [:name, :locale, :auto_close, :remind_interval, :block_after_refusal]
   defstruct [
     :name,
@@ -6,11 +10,37 @@ defmodule Signex.Structures.Envelope do
     :auto_close,
     :remind_interval,
     :block_after_refusal,
+    :deadline_at,
     :default_subject,
     :default_message
   ]
 
+  @required_keys @enforce_keys
+
+  @type t :: %__MODULE__{
+          name: String.t(),
+          deadline_at: String.t(),
+          locale: String.t(),
+          auto_close: boolean(),
+          remind_interval: String.t(),
+          block_after_refusal: boolean(),
+          default_subject: String.t() | nil,
+          default_message: String.t() | nil
+        }
+
+  @spec build(map()) :: {:ok, t()} | {:error, String.t()}
   def build(attrs) do
-    {:ok, struct(__MODULE__, attrs)}
+    with :ok <- validate_required(attrs) do
+      {:ok, struct(__MODULE__, attrs)}
+    else
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  defp validate_required(attrs) do
+    case Enum.all?(@required_keys, &Map.has_key?(attrs, &1)) do
+      true -> :ok
+      false -> {:error, "Missing required fields: #{inspect(@required_keys -- Map.keys(attrs))}"}
+    end
   end
 end
