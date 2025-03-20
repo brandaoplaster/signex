fb1d798dca6938eb80c6429034e142b10ba17c3a
brandaoplaster <brandaoplaster@gmail.com>
2025-03-19 22:34:33 -0300

    feat: Add spec in functions

--- lib/signex/structures/document.ex
+++ lib/signex/structures/document.ex
@@ -12,6 +12,7 @@ defmodule Signex.Structures.Document do
           content_base64: String.t()
         }
 
+  @spec build(map()) :: {:ok, t()} | {:error, String.t()}
   def build(attrs) do
     with :ok <- check_required(attrs),
          :ok <- check_types(attrs) do
@@ -21,6 +22,7 @@ defmodule Signex.Structures.Document do
     end
   end
 
+  @spec check_required(map()) :: :ok | {:error, String.t()}
   defp check_required(attrs) do
     missing = @required_keys -- Map.keys(attrs)
 
@@ -30,6 +32,7 @@ defmodule Signex.Structures.Document do
     end
   end
 
+  @spec check_types(map()) :: :ok | {:error, String.t()}
   defp check_types(attrs) do
     with {:ok, _} <- validate_field(attrs, :filename, &is_binary/1),
          {:ok, _} <- validate_field(attrs, :content_base64, &is_binary/1) do
@@ -37,8 +40,10 @@ defmodule Signex.Structures.Document do
     end
   end
 
+  @spec validate_field(map(), atom(), (any() -> boolean())) :: {:ok, any()} | {:error, String.t()}
   defp validate_field(attrs, key, validator) do
     value = Map.get(attrs, key)
+
     case validator.(value) do
       true -> {:ok, value}
       false -> {:error, "invalid type for #{key}"}
