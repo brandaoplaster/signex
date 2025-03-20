bb8db25629a6468dc03f815a263f118b47db5b97
brandaoplaster <brandaoplaster@gmail.com>
2025-03-19 22:32:00 -0300

    feat: Add functions check_types and check_required

--- lib/signex/structures/document.ex
+++ lib/signex/structures/document.ex
@@ -5,13 +5,43 @@ defmodule Signex.Structures.Document do
   @enforce_keys [:filename, :content_base64]
   defstruct [:filename, :content_base64]
 
+  @required_keys @enforce_keys
+
   @type t :: %__MODULE__{
           filename: String.t(),
           content_base64: String.t()
         }
 
-
   def build(attrs) do
-    {:ok, struct(__MODULE__, attrs)}
+    with :ok <- check_required(attrs),
+         :ok <- check_types(attrs) do
+      {:ok, struct(__MODULE__, attrs)}
+    else
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  defp check_required(attrs) do
+    missing = @required_keys -- Map.keys(attrs)
+
+    case missing do
+      [] -> :ok
+      keys -> {:error, "Missing required fields: #{inspect(keys)}"}
+    end
+  end
+
+  defp check_types(attrs) do
+    with {:ok, _} <- validate_field(attrs, :filename, &is_binary/1),
+         {:ok, _} <- validate_field(attrs, :content_base64, &is_binary/1) do
+      :ok
+    end
+  end
+
+  defp validate_field(attrs, key, validator) do
+    value = Map.get(attrs, key)
+    case validator.(value) do
+      true -> {:ok, value}
+      false -> {:error, "invalid type for #{key}"}
+    end
   end
 end
